a0be990f8fc0f16a35e699201b156733b05d83a0
Pedro Fernandes <v-pedro.fernandes@bryj.ai>
2024-08-07 15:37:01 +0100

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "Build-V3-Plugin-Monitoring-ios", url: "https://bryj-sdks.s3.eu-west-1.amazonaws.com/build/plugins/Build_V3_Plugin_Monitoring_ios/beta/1.0.1-beta.79/Build_V3_Plugin_Monitoring_ios_1.0.1-beta.79.xcframework.zip, checksum: "1c8ff84f9f6856cd96e946f9c22366ca195720f6d4e7c975c8424e4e6ff69f86")
+        .binaryTarget(name: "Build-V3-Plugin-Monitoring-ios", url: "https://bryj-sdks.s3.eu-west-1.amazonaws.com/build/plugins/Build_V3_Plugin_Monitoring_ios/beta/1.0.1-beta.79/Build_V3_Plugin_Monitoring_ios_1.0.1-beta.79.xcframework.zip", checksum: "1c8ff84f9f6856cd96e946f9c22366ca195720f6d4e7c975c8424e4e6ff69f86")
     ]
 )
