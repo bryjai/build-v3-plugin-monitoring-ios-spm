c90f43d8aa89ac1e15ceab6ef082aecc7267a5e9
Pedro Fernandes <v-pedro.fernandes@bryj.ai>
2024-08-26 09:37:17 +0100

    Update beta package served to 1.0.2-beta.82

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "Build-V3-Plugin-Monitoring-ios", url: "https://bryj-sdks.s3.eu-west-1.amazonaws.com/build/plugins/Build_V3_Plugin_Monitoring_ios/beta/1.0.2-beta.81/Build_V3_Plugin_Monitoring_ios_1.0.2-beta.81.xcframework.zip", checksum: "3a7edd5f866c9a7046e36acc698f1586e160361c3bf2fe3ae89d4284659f20ea")
+        .binaryTarget(name: "Build-V3-Plugin-Monitoring-ios", url: "https://bryj-sdks.s3.eu-west-1.amazonaws.com/build/plugins/Build_V3_Plugin_Monitoring_ios/beta/1.0.2-beta.82/Build_V3_Plugin_Monitoring_ios_1.0.2-beta.82.xcframework.zip", checksum: "65e358ad82aa10a4d9533846d4ab546eaae273638879d73f1da646145976e541")
     ]
 )
